5caca355affbeaadbf73884d10c23999fb7fe6d9
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2024-02-14 15:59:04 +0530

    pakage update react-router , redux

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../assets/home.css';
+function Home(params) {
+  
+  return (
+    <div>
+    <h1 className="home">Home Components</h1>
+    </div>
+  )
+}
+
+export default Home;

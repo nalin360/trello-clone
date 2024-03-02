08d7c39c921677456392ec13bece02bcf3f6b669
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2024-03-02 18:50:26 +0530

    feat: setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Home from './pages/Home/Home'
+import Home from './pages/Home'
+import { BrowserRouter, Routes ,Route} from 'react-router-dom'
+import LoginForm from './components/LoginSignUp/LoginForm'
+
 // import './App.css'
 
 function App() {
-  return (
-    <>
-    <Home />
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/login' element={<LoginForm/>}/>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App

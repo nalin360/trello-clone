f94662b6005674d8fcc25cd57b3719518d2c98f6
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2024-03-05 14:47:23 +0530

    chore: redux , redux/toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import store from './Redux/store/index.js'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>,
 )

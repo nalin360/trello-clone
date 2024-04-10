1b0a9e8459e89315dde78cc9d3ce89581145c2e0
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2024-04-10 15:51:58 +0530

    material ui and heroicons added dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import LoginForm from "./components/LoginSignUp/LoginForm";
 import Register from "./components/LoginSignUp/Register";
+import Dashboard from "./pages/Dashboard";
+
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginForm/>} />
         <Route path="/signup" element={<Register/>} />
+        <Route path="/dashboard" element={<Dashboard />} >
+          <Route path="board" element={<div>Board</div>} />
+          <Route path="analytics" element={<div>Analytics</div>} />
+        </Route>
       </Routes>
     </>
   );
